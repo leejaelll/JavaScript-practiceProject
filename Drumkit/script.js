52a4a3862f8c007e12a6e63c6490b5a6baaaa794
leejaelll <jayaom91076@gmail.com>
2023-01-20 11:17:44 +0900

    feat: Create addEventListener handler function
    - keydown: 키보드로 눌렀을 때 playSound 함수가 실행되도록 구현
    - mousedown: 마우스로 눌렀을 때 playSound 함수가 실행되도록 구현
    - transitioned: transform이 끝났을 때 propertyName의 프로퍼티로 확인이 가능하다. 이때 playing 클래스를 remove하도록 구현

--- Drumkit/script.js
+++ Drumkit/script.js
@@ -1,4 +1,4 @@
-const $wrapper = document.querySelector('.wrapper');
+const $keys = document.querySelector('.keys');
 
 const drumSounds = [
   { keyboard: 'Q', key: 81, sound: 'clap.wav' },
@@ -15,8 +15,7 @@ const soundsRoot = 'assets/sounds/';
 
 const render = (drumSounds) => {
   //prettier-ignore
-  $wrapper.innerHTML =  `
-  <div class="keys">
+  $keys.innerHTML =  `
     ${drumSounds.map(({keyboard, key, sound}) =>`
      <div class="key" data-key="${key}">
         <div class="label">
@@ -24,10 +23,35 @@ const render = (drumSounds) => {
             <p class="sound">${sound.replace(/.wav/g, '')}</p>
         </div>
         <audio data-key="${key}" src="${soundsRoot}${sound}"></audio>
-     </div>`).join('')}
-  </div>`
+     </div>`).join('')}`
+};
+
+const playSound = (keycode) => {
+  const $audio = document.querySelector(`audio[data-key="${keycode}"]`);
+  const $key = document.querySelector(`div[data-key="${keycode}"]`);
+  if ($key && $audio) {
+    $key.classList.add('playing');
+    $audio.currentTime = 0;
+    $audio.play();
+  }
 };
 
 window.addEventListener('DOMContentLoaded', () => {
   render(drumSounds);
+});
+
+window.addEventListener('keydown', (e) => {
+  playSound(e.keyCode);
+});
+
+$keys.addEventListener('mousedown', (e) => {
+  if (!e.target.closest('.key')) return;
+
+  playSound(e.target.closest('.key').dataset.key);
+});
+
+$keys.addEventListener('transitionend', (e) => {
+  if (e.propertyName === 'transform') {
+    e.target.classList.remove('playing');
+  }
 });
